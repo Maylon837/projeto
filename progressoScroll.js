af0419c07a4c71a37bb00e49c88d87f61bf82052
CamilaLiborio <camilaliborio33@gmail.com>
2025-11-12 23:37:16 -0300

    .

---
+++ progressoScroll.js
@@ -0,0 +1,18 @@
+window.onscroll = function() {
+    calcularProgressoDeLeitura(); // carrega a função toda vez que o usuário rolar a pg.
+    };
+
+    function calcularProgressoDeLeitura() {
+                
+        const alturaTotalConteudo = document.body.scrollHeight - window.innerHeight;
+
+        const posicaoAtual = document.documentElement.scrollTop || document.body.scrollTop;
+
+        const porcentagemLida = (posicaoAtual / alturaTotalConteudo) * 100;
+
+        const barraDeProgresso = document.getElementById("progressoBarra");
+
+        if (barraDeProgresso) {
+                 barraDeProgresso.style.width = porcentagemLida.toFixed(2) + "%";
+            }
+        }
